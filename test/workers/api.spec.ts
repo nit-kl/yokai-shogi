9f98b11b82338b8d32f6d1d644e045d6a08dd06b
ReoKojima <kojileo0178@gmail.com>
2026-07-24 21:09:23 +0900

    お知らせ更新

--- test/workers/api.spec.ts
+++ test/workers/api.spec.ts
@@ -619,12 +619,18 @@ describe('共通', () => {
     const r = await api('/v1/announcements');
     expect(r.status).toBe(200);
     expect(r.body.announcements[0]).toMatchObject({
-      id: '2026-07-19-random-match-anytime',
+      id: '2026-07-24-hyakki-frontier',
       type: 'update',
       priority: 'high',
-      title: 'ランダムマッチをいつでも利用できるようにしました',
+      title: 'ソロを百鬼夜行の連戦施設に刷新しました',
     });
     expect(r.body.announcements).toEqual(expect.arrayContaining([
+      expect.objectContaining({
+        id: '2026-07-19-random-match-anytime',
+        type: 'update',
+        priority: 'high',
+        title: 'ランダムマッチをいつでも利用できるようにしました',
+      }),
       expect.objectContaining({
         id: '2026-07-19-hyakki-hasha-kyubi',
         type: 'campaign',
@@ -657,7 +663,7 @@ describe('共通', () => {
     const r = await api('/v1/announcements');
     expect(r.status).toBe(200);
     expect(r.body.announcements[0]).toMatchObject({
-      id: '2026-07-19-random-match-anytime',
+      id: '2026-07-24-hyakki-frontier',
       type: 'update',
       priority: 'high',
     });
